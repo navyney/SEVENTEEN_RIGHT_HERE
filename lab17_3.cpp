d5250f9da7bb3c878ce4858880cf57273022df0d
navyney <141761309+navyney@users.noreply.github.com>
2024-02-09 14:58:05 +0700

    completed

--- lab17_3.cpp
+++ lab17_3.cpp
@@ -9,16 +9,35 @@ void showVector(vector<int>);
 int dotProduct(vector<int>,vector<int>);
 
 int main(){
-	srand(time(0));
-	vector<int> x = randomVector(5);
-	vector<int> y = randomVector(5);
-	int z = dotProduct(x,y);
-	showVector(x);
-	cout << " . ";
-	showVector(y);
-	cout << " = " << z;
-	
-	return 0;
+    srand(time(0));
+    vector<int> x = randomVector(5);
+    vector<int> y = randomVector(5);
+    int z = dotProduct(x,y);
+    showVector(x);
+    cout << " . ";
+    showVector(y);
+    cout << " = " << z;
+    
+    return 0;
 }
 
-//Write Definition of randomVector() showVector() and dotProduct() here
+vector<int> randomVector(int N){    
+    vector<int> v;
+    for(int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
+}
+
+void showVector(vector<int> v){
+    cout << "[";
+    for(unsigned int i = 0; i < v.size(); i++){
+        cout << v[i];
+        if(i == v.size() - 1) cout << "]";
+        else cout << " ";
+    }
+}
+
+int dotProduct(vector<int> u, vector<int> v){
+    int sum = 0;
+    for(unsigned int i = 0; i < u.size(); i++) sum += u[i] * v[i];    
+    return sum;
+}
